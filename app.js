d88752bdb9af82e34b72b1156c3663670ee34ad7
Louis Savoie <thelousavoie@gmail.com>
2024-02-19 12:23:16 -0500

    Add database
    Adds MongoDB, Mongoose, DotENV for database usage.

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
 // REQUIRES
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose')
+require('dotenv').config()
+
+//GLOBAL VARS
 const port = 3000
+
+// CONNECT MONGOOSE TO MONGODB
+mongoose.connect(process.env.DATABASEURL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(console.log('Mongoose connected to MongoDB'))
+.catch(error => console.log(error.message))
+
 
 // APP SETUP
 app.set('view engine', 'ejs');
