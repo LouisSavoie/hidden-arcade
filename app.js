27f95f055124f0db4438f3ebc1e7e50a37a4bbc6
Louis Savoie <thelousavoie@gmail.com>
2024-02-22 10:13:32 -0500

    Add sessions
    Adds passport sessions with local strategy. Installs psssport, passport-local, passport-local-mongoose, and express-sessions. Adds all nessisary config to app.js and to User mongoose model.

--- app.js
+++ app.js
@@ -2,6 +2,8 @@
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
+const passport = require("passport")
+const LocalStrategy = require("passport-local")
 require('dotenv').config()
 
 //GLOBAL VARS
@@ -20,6 +22,25 @@ app.set('view engine', 'ejs');
 // REQUIRE AND USE ROUTES
 require('fs').readdirSync('./routes/').forEach((file) => {
   app.use(require('./routes/' + file))
+})
+
+// PASSPORT CONFIG
+app.use(require("express-session")({
+  secret: process.env.EXPSESSSCT,
+  resave: false,
+  saveUninitialized: false
+}))
+app.use(passport.initialize())
+app.use(passport.session())
+passport.use(new LocalStrategy(User.authenticate()))
+passport.serializeUser(User.serializeUser())
+passport.deserializeUser(User.deserializeUser())
+
+app.use(function(req, res, next){
+  res.locals.currentUser = req.user
+  // res.locals.error = 
+  // res.locals.success = 
+  next()
 })
 
 // PORT LISTENING
