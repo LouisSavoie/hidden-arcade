ea9f7e4a7e874148225e02d5884d527f8949f260
Louis Savoie <thelousavoie@gmail.com>
2024-02-23 11:04:08 -0500

    Add placeholder register page
    Adds a placeholder register page and get route.

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,14 @@ const router = express.Router();
 
 // INDEX ROUTES
 
+// GET index
 router.get('/', (req, res) => {
   res.render('index')
+})
+
+// GET register
+router.get("/register", (req, res) => {
+  res.render("register")
 })
 
 module.exports = router
