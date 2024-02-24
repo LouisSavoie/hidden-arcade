15e0182aa2565fb9ab6438724069c4af9029c69a
Louis Savoie <thelousavoie@gmail.com>
2024-02-24 17:33:13 -0500

    Add register post route
    Adds register post route to index.js routes, installs and sets up body-parser to parse form data.

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // REQUIRES
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 const passport = require("passport")
 const LocalStrategy = require("passport-local")
@@ -17,6 +18,7 @@ mongoose.connect(process.env.DATABASEURL, {})
 const User = require('./models/user')
 
 // APP SETUP
+app.use(bodyParser.urlencoded({extended: true}))
 app.set('view engine', 'ejs');
 
 // REQUIRE AND USE ROUTES
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
-const express = require("express");
-const router = express.Router();
+const express = require('express')
+const router = express.Router()
+const passport = require('passport')
+const User = require('../models/user')
 
 // INDEX ROUTES
 
@@ -9,8 +11,24 @@ router.get('/', (req, res) => {
 })
 
 // GET register
-router.get("/register", (req, res) => {
-  res.render("register")
+router.get('/register', (req, res) => {
+  res.render('register')
+})
+
+// POST register
+router.post('/register', (req, res) => {
+  // get data from form
+  let newUser = new User({username: req.body.username})
+  // add data to User model and add to DB
+  User.register(newUser, req.body.password, function(err, user) {
+      if(err){
+          return res.render('/register')
+      }
+      // authenticate user login through passport and redirect to index view
+      passport.authenticate('local')(req, res, function() {
+          res.redirect('/')
+      })
+  })
 })
 
 module.exports = router
