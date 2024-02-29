49e6a28acdcbbe45c53deb39a9c0daea98a7530b
Louis Savoie <thelousavoie@gmail.com>
2024-02-28 19:27:49 -0500

    Add log out link
    Adds a placholder log out link to the index view and placeholder GET log out route to index routes for testing.

--- routes/index.js
+++ routes/index.js
@@ -41,4 +41,9 @@ router.post('/login', passport.authenticate('local', {failureRedirect: '/login'}
     res.redirect('/')
 })
 
+// GET log out
+router.get('/logout', (req, res) => {
+  res.send('You are logged out')
+})
+
 module.exports = router
